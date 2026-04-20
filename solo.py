aa1d0e94bd3a44da25d8d6c0f619f120c3c6c16d
Ismailyoussef111 <youssf2395ismail@gmail.com>
2026-04-20 13:15:17 +0200

    Update solo.py

--- solo.py
+++ solo.py
@@ -4,26 +4,7 @@ from tkinter import filedialog
 from PIL import Image, ImageTk 
 import tkinter as tk
 from random import shuffle
-def deck():
-    groupe = [" coeur", " pick", " trefle", " carreau"]
-    deck_carte = []
-    special = ["As", "Valet", "Roi", "Reine"]
-    for nom in groupe:
-        for majeur in special:
-            deck_carte.append(majeur + nom)
-        for valeur in range(2, 11):
-            deck_carte.append(str(valeur) + nom)
-    shuffle(deck_carte)
-    return deck_carte 
-def valeur_carte(carte):
-        if "Roi" in carte or "Valet" in carte or "Reine" in carte or "10" in carte:  
-            return 10
-        if "As" in carte:
-            return 1                 
-        for n in range(2, 10):
-            if str(n) in carte:
-                return n
-        return 0
+
 
 import tkinter as tk
 
