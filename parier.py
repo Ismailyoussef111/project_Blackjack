0484ab29ffa3d705274166440917e79398c81afa
Ismailyoussef111 <youssf2395ismail@gmail.com>
2026-04-30 11:16:56 +0200

    Update parier.py
    correction de probleme d'import

--- parier.py
+++ parier.py
@@ -2,22 +2,22 @@
 # on met sa touten haut du fichier
 argent_joueur = 1000  # par defaut j'ai mis 1000 euros
 
-
-
+import tkinter as tk
+import solo as sl
 
 #Dans la fonction solo : 
-    global argent_joueur 
-    mise_actuelle = tk.IntVar(value=0) # On crée une boîte pour stocker la mise du tour
+global argent_joueur 
+mise_actuelle = tk.IntVar(value=0) # On crée une boîte pour stocker la mise du tour
 
-    def valider_mise():
+def valider_mise():
         global argent_joueur
         try:
             m = int(entree_mise.get()) # On lit le nombre écrit dans la case blanche quon a entrer 
             if 0 < m <= argent_joueur: # Si on as assez d'argent
                 mise_actuelle.set(m)    # On enregistre la mise
                 label_pari.config(text=f"Mise : {m}€", fg="#2ecc71") # On écrit en vert
-                btn_tirage.config(state="normal") # On ALLUME le bouton reprendre
-                btn_reste.config(state="normal")   # On ALLUME le bouton garder
+                sl.btn_tirage.config(state="normal") # On ALLUME le bouton reprendre
+                sl.btn_reste.config(state="normal")   # On ALLUME le bouton garder
                 btn_valider.config(state="disabled") # On bloque le bouton MISER
             else:
                 label_pari.config(text="Montant invalide", fg="#e74c3c") # Erreur en rouge
@@ -26,35 +26,32 @@ argent_joueur = 1000  # par defaut j'ai mis 1000 euros
 # encore dans solo :
     
     # Texte qui affiche notre argent total en haut à droite
-    label_argent = tk.Label(fensolo, text=f"Argent : {argent_joueur}€", font=("Rockwell", 25), fg="white", bg="#142e14")
-    label_argent.place(relx=0.8, rely=0.05, anchor="center")
+label_argent = tk.Label(sl.fensolo, text=f"Argent : {argent_joueur}€", font=("Rockwell", 25), fg="white", bg="#142e14")
+label_argent.place(relx=0.8, rely=0.05, anchor="center")
 
     # Case blanche pour taper le montant de la mise
-    entree_mise = tk.Entry(fensolo, font=("Rockwell", 18))
-    entree_mise.place(relx=0.5, rely=0.75, anchor="center")
+entree_mise = tk.Entry(sl.fensolo, font=("Rockwell", 18))
+entree_mise.place(relx=0.5, rely=0.75, anchor="center")
     
     # Bouton pour cliquer et valider la mise
-    btn_valider = tk.Button(fensolo, text="MISER", command=valider_mise, bg="#52be8c", width=10)
-    btn_valider.place(relx=0.5, rely=0.82, anchor="center")
+btn_valider = tk.Button(sl.fensolo, text="MISER", command=valider_mise, bg="#52be8c", width=10)
+btn_valider.place(relx=0.5, rely=0.82, anchor="center")
 
     #  texte qui nous demande d'entrer notre mise au-dessus de la case
-    label_pari = tk.Label(fensolo, text="Entrez votre mise", font=("Rockwell", 15), fg="white", bg="#142e14")
-    label_pari.place(relx=0.5, rely=0.7, anchor="center")
+label_pari = tk.Label(sl.fensolo, text="Entrez votre mise", font=("Rockwell", 15), fg="white", bg="#142e14")
+label_pari.place(relx=0.5, rely=0.7, anchor="center")
 
 # dans fin croupier() et rester()
     
     # Dans fin_croupier(), j'ai ajouté cette ligne pour rafraîchir l'affichage de notre argent
-    label_argent.config(text=f"Argent : {argent_joueur}€") 
+label_argent.config(text=f"Argent : {argent_joueur}€") 
 
     # Dans rester() just je calcule notre argent final genre soit gagne soit perdu :
-    sj, sc = val_main(joueur), val_main(croupier) # Les scores
+sj, sc = sl.val_main(sl.joueur), sl.val_main(sl.croupier) # Les scores
     
-    if sj <= 21 and (sc > 21 or sj > sc):
-        argent_joueur += mise_actuelle.get() # On AJOUTE la mise si on gagnes
-    elif sj < sc or sj > 21:
-        argent_joueur -= mise_actuelle.get() # On RETIRE la mise si on perds
+if sj <= 21 and (sc > 21 or sj > sc):
+    argent_joueur += mise_actuelle.get()*2 # On AJOUTE la mise si on gagnes
+elif sj < sc or sj > 21:
+    argent_joueur -= mise_actuelle.get() # On RETIRE la mise si on perds
     # Si égalité, on ne fait rien
-
-
-#question pr celui qui as tout lu quand on a plus d'argent on peut plus jouait prcq faut parier avant on fait comment??????????????????????????????
 
