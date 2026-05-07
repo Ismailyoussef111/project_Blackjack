58aef3379111194d0701fec3c1296d8f8bd7c35e
Youcef <youcefahmim33@gmail.com>
2026-05-08 00:27:12 +0200

    Update function definition

--- deck.py
+++ deck.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from random import shuffle
 
-def deck():
+def deck() :
     groupe = [" coeur", " pick", " trefle", " carreau"]
     deck_carte = []
     special = ["As", "Valet", "Roi", "Reine"]
